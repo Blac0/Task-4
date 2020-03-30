ae74d806805e8fea4821cd29dad679b529cb7d10
Arunim Havelia <arunimrockz97@gmail.com>
2020-03-30 21:29:00 +0530

    2.2

--- TwoActivities/app/src/main/java/com/example/twoactivities/MainActivity.java
+++ TwoActivities/app/src/main/java/com/example/twoactivities/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.twoactivities;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -15,15 +17,78 @@ public class MainActivity extends AppCompatActivity {
     public static final int TEXT_REQUEST = 1;
     private TextView rt;
     private TextView rts;
+    public static final String MYTAG = "Sorc";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(MYTAG, "-------");
+        Log.d(MYTAG, "onCreate");
 
         et= findViewById(R.id.et_main);
         rt= findViewById(R.id.msg);
         rts= findViewById(R.id.msr);
+
+        if(savedInstanceState!=null){
+            boolean isVisible = savedInstanceState.getBoolean("reply_visible");
+            if(isVisible)
+            {
+                rts.setVisibility(View.VISIBLE);
+                rt.setText(savedInstanceState.getString("reply_text"));
+                rt.setVisibility(View.VISIBLE);
+            }
+        }
+
+
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(MYTAG, "onPause");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(MYTAG, "onRestart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(MYTAG, "onResume");
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(MYTAG, "onStart");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(MYTAG, "onStop");
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if(rts.getVisibility()==View.VISIBLE)
+        {
+            outState.putBoolean("reply_visible", true);
+            outState.putString("reply_text" , rt.getText().toString());
+
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(MYTAG, "onDestroy");
     }
 
     public void launchSecondActivity(View view) {
